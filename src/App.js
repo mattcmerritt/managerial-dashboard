fe2612864e95ee69b9bf54747dfce5044cd139a4
mattcmerritt <mattcmerritt@gmail.com>
2022-02-16 06:57:11 -0500

    basic saving files using express backend

--- src/App.js
+++ src/App.js
@@ -1,43 +1,83 @@
+import React, { Component } from "react";
 import './App.css';
 import Navbar from './Navbar';
+import FileUploader from "./FileUploader";
 import Graph from './Graph';
 
-function App() {
-  const graphData = [
-    {
-      link: 'https://mikepmerritt.github.io/DashboardTesting/pie_chart.html',
-      h: '525',
-      w: '30%'
-    },
-    {
-      link: 'https://mikepmerritt.github.io/DashboardTesting/pie_chart_actionable.html',
-      h: '525',
-      w: '30%'
-    },
-    {
-      link: 'https://mikepmerritt.github.io/DashboardTesting/pillar_chart.html',
-      h: '525',
-      w: '30%'
-    }
-  ];
+class App extends Component {
 
-  return (
-    <div className="App">
-      <Navbar />
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
 
-      <div className="content">
-        <Graph link={graphData[0].link} h={graphData[0].h} w={graphData[0].w} />
-        <Graph link={graphData[1].link} h={graphData[1].h} w={graphData[1].w} />
-        <Graph link={graphData[2].link} h={graphData[2].h} w={graphData[2].w} />
-        <Graph link={graphData[0].link} h={graphData[0].h} w={graphData[0].w} />
-        <Graph link={graphData[1].link} h={graphData[1].h} w={graphData[1].w} />
-        <Graph link={graphData[2].link} h={graphData[2].h} w={graphData[2].w} />
-        <Graph link={graphData[0].link} h={graphData[0].h} w={graphData[0].w} />
-        <Graph link={graphData[1].link} h={graphData[1].h} w={graphData[1].w} />
-        <Graph link={graphData[2].link} h={graphData[2].h} w={graphData[2].w} />
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  uploadFile() {
+    var data = { content: "example" };
+
+    fetch("http://localhost:9000/upload", {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data)
+    })
+  }
+
+  UNSAFE_componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    const graphData = [
+      {
+        link: 'https://mikepmerritt.github.io/DashboardTesting/pie_chart.html',
+        h: '525',
+        w: '30%'
+      },
+      {
+        link: 'https://mikepmerritt.github.io/DashboardTesting/pie_chart_actionable.html',
+        h: '525',
+        w: '30%'
+      },
+      {
+        link: 'https://mikepmerritt.github.io/DashboardTesting/pillar_chart.html',
+        h: '525',
+        w: '30%'
+      }
+    ];
+
+    return (
+      <div className="App">
+        <Navbar />
+
+        <FileUploader />
+        <p className="App-intro">{this.state.apiResponse}</p>
+
+        <div className="content">
+          <Graph link={graphData[0].link} h={graphData[0].h} w={graphData[0].w} />
+          <Graph link={graphData[1].link} h={graphData[1].h} w={graphData[1].w} />
+          <Graph link={graphData[2].link} h={graphData[2].h} w={graphData[2].w} />
+          <Graph link={graphData[0].link} h={graphData[0].h} w={graphData[0].w} />
+          <Graph link={graphData[1].link} h={graphData[1].h} w={graphData[1].w} />
+          <Graph link={graphData[2].link} h={graphData[2].h} w={graphData[2].w} />
+          <Graph link={graphData[0].link} h={graphData[0].h} w={graphData[0].w} />
+          <Graph link={graphData[1].link} h={graphData[1].h} w={graphData[1].w} />
+          <Graph link={graphData[2].link} h={graphData[2].h} w={graphData[2].w} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
