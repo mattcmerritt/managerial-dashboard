2c798541ece8d55b6c9d4e695e189e86a1cdd5a7
mattcmerritt <mattcmerritt@gmail.com>
2022-02-16 01:40:22 -0500

    displaying a couple of graphs on the page using iframes and Graph components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Graph from './Graph';
 
 function App() {
+  const graphData = [
+    {
+      link: 'https://mikepmerritt.github.io/DashboardTesting/pie_chart.html',
+      h: '525',
+      w: '30%'
+    },
+    {
+      link: 'https://mikepmerritt.github.io/DashboardTesting/pie_chart_actionable.html',
+      h: '525',
+      w: '30%'
+    },
+    {
+      link: 'https://mikepmerritt.github.io/DashboardTesting/pillar_chart.html',
+      h: '525',
+      w: '30%'
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      <div className="content">
+        <Graph link={graphData[0].link} h={graphData[0].h} w={graphData[0].w} />
+        <Graph link={graphData[1].link} h={graphData[1].h} w={graphData[1].w} />
+        <Graph link={graphData[2].link} h={graphData[2].h} w={graphData[2].w} />
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Managerial Dashboard</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
