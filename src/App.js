a96c94733da39a742cd18010df88969c50cd33b8
mattcmerritt <mattcmerritt@gmail.com>
2022-02-16 04:09:59 -0500

    making graphs appear in windows and testing that multiple windows can be displayed

--- src/App.js
+++ src/App.js
@@ -29,6 +29,12 @@ function App() {
         <Graph link={graphData[0].link} h={graphData[0].h} w={graphData[0].w} />
         <Graph link={graphData[1].link} h={graphData[1].h} w={graphData[1].w} />
         <Graph link={graphData[2].link} h={graphData[2].h} w={graphData[2].w} />
+        <Graph link={graphData[0].link} h={graphData[0].h} w={graphData[0].w} />
+        <Graph link={graphData[1].link} h={graphData[1].h} w={graphData[1].w} />
+        <Graph link={graphData[2].link} h={graphData[2].h} w={graphData[2].w} />
+        <Graph link={graphData[0].link} h={graphData[0].h} w={graphData[0].w} />
+        <Graph link={graphData[1].link} h={graphData[1].h} w={graphData[1].w} />
+        <Graph link={graphData[2].link} h={graphData[2].h} w={graphData[2].w} />
       </div>
     </div>
   );
--- src/Graph.js
+++ src/Graph.js
@@ -1,9 +1,4 @@
 const Graph = (props) => {
-    // const link = props["link"], h = props["h"], w = props["w"];
-
-    // console.log(props);
-    // console.log(link + "\n" + h + "\n" + w);
-
     return (
         <iframe id="igraph" scrolling="no" style={{border:'none'}} seamless="seamless" src={props.link} height={props.h} width={props.w}></iframe>
     );
