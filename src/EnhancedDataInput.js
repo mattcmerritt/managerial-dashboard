e7418dc7878095f0dc73436d4a0cfd7df11d4b54
mattcmerritt <mattcmerritt@gmail.com>
2022-04-12 12:33:14 -0400

    transferred field selection from DataInput to EnhancedDataInput

--- src/EnhancedDataInput.js
+++ src/EnhancedDataInput.js
@@ -9,6 +9,7 @@ class EnhancedDataInput extends Component {
         // binding functions to class
         this.ProcessDataset = this.ProcessDataset.bind(this);
         this.SelectSteps = this.SelectSteps.bind(this);
+        this.RemoveUnchecked = this.RemoveUnchecked.bind(this);
         this.ClassifySteps = this.ClassifySteps.bind(this);
         this.PerformStep = this.PerformStep.bind(this);
         this.ContinueProcess = this.ContinueProcess.bind(this);
@@ -75,17 +76,136 @@ class EnhancedDataInput extends Component {
 
     // allow the user to remove fields that are not part of the process (like ID)
     // saves edited dataset
+    // button will need to be pressed to get to next step
     async SelectSteps(dataset) {
-        console.log("Selecting steps");
         // TODO: reimplement selecting steps with the checkboxes
+        const parentDiv = document.getElementById(this.props.group + "DataInSettings");
+
+        // step 1: remove unecessary columns from the dataset
+        const fieldList = document.createElement("ul");
+        fieldList.id = "outerList";
+        fieldList.style = "list-style: none";
+
+        // creating a parent checkbox to mark all as checked
+        const allFields = document.createElement("li");
+        const allCheckbox = document.createElement("input");
+        allCheckbox.type = "checkbox";
+        allCheckbox.id = "allFields";
+
+        const allLabel = document.createElement("label");
+        allLabel.for = "allFields";
+        allLabel.innerHTML = "Fields Found:";
+
+        allFields.appendChild(allCheckbox);
+        allFields.appendChild(allLabel);
+        fieldList.appendChild(allFields);
+
+        // creating an inner list to have the child checkboxes
+        const innerList = document.createElement("ul");
+        innerList.id = "fieldList";
+        innerList.style = "list-style: none";
+
+        for (let list of dataset) {
+            // building out the checkboxes
+            const item = document.createElement("li");
+
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.id = list.name;
+
+            const label = document.createElement("label");
+            label.for = list.name;
+            label.innerHTML = `${list.name} (${list.data.length} data points)`;
+
+            // putting the components into a list item and adding that item to the list
+            item.appendChild(checkbox);
+            item.appendChild(label);
+            innerList.appendChild(item);
+        }
+
+        // putting the lists into the div
+        fieldList.appendChild(innerList);
+        parentDiv.appendChild(fieldList);
+
+        let boxesChecked = 0; // number of boxes checked
+
+        // making the parent checkbox check all of the child checkboxes
+        allCheckbox.addEventListener("click", (e) => {
+            const newState = document.querySelector(`#outerList input[id="allFields"]`).checked;
+
+            for (let list of dataset) {
+                const checkbox = document.querySelector(`#fieldList input[id="${list.name}"]`);
+                checkbox.checked = newState;
+            }
+
+            boxesChecked = newState ? dataset.length : 0;
+        });
+
+        // adding listeners to each child checkbox to make them uncheck the parent if unchecked
+        for (let list of dataset) {
+            const checkbox = document.querySelector(`#fieldList input[id="${list.name}"]`);
+
+            checkbox.addEventListener("click", () => {
+                const newState = checkbox.checked;
+
+                const parentBox = document.querySelector(`#outerList input[id="allFields"]`);
+                if (newState) {
+                    if (boxesChecked === 0) {
+                        // change parent to indeterminate
+                        parentBox.checked = false;
+                        parentBox.indeterminate = true;
+                    }
+                    boxesChecked++;
+                    if (boxesChecked === dataset.length) {
+                        // check parent
+                        parentBox.indeterminate = false;
+                        parentBox.checked = true;
+                    }
+                } else {
+                    boxesChecked--;
+                    parentBox.indeterminate = true;
+                    parentBox.checked = false;
+                    if (boxesChecked === 0) {
+                        // uncheck parent
+                        parentBox.indeterminate = false;
+                        parentBox.checked = false;
+                    }
+                }
+            });
+        }
+
         // saving dataset for next step in the process
         this.setState({dataset: dataset});
+    }
+
+    // based on the checkboxes, remove the fields that are not checked from the dataset
+    // saves edited dataset
+    // should start next step without button
+    async RemoveUnchecked(dataset) {
+        const parentDiv = document.getElementById(this.props.group + "DataInSettings");
+
+        // reading checkmarks and saving as property
+        for (let i = 0; i < dataset.length; i++) {
+            const currentBox = document.querySelector(`#fieldList input[id="${dataset[i].name}"]`);
+            if (!currentBox.checked) {
+                dataset.splice(i, 1); // remove list from dataset
+                i--;
+            }
+        }
+
+        // remove the lists for this step
+        parentDiv.removeChild(document.querySelector("#outerList"));
+
+        // save dataset, move on to next step
+        this.setState({dataset: dataset});
+        this.ContinueProcess();
     }
 
     // allowing the user to choose categories for each step
     // saves edited dataset
     async ClassifySteps(dataset) {
         console.log("Classifying steps");
+        console.log(dataset);
         // TODO: reimplement classifications with dropdown selections
         // saving dataset for next step in the process
         this.setState({dataset: dataset});
@@ -105,7 +225,6 @@ class EnhancedDataInput extends Component {
     // returns nothing
     async ProcessDataset(e) {
         // removing the form component once changed
-        console.log(this);
         const form = document.getElementById(this.props.group + "DataInForm");
         form.remove();
 
@@ -121,7 +240,7 @@ class EnhancedDataInput extends Component {
         this.setState(
             {
                 current: 0, 
-                steps: [this.SelectSteps, this.ClassifySteps, this.TerminateProcess], 
+                steps: [this.SelectSteps, this.RemoveUnchecked, this.ClassifySteps, this.TerminateProcess], 
                 dataset: initialDataset
             }
         );
@@ -142,7 +261,7 @@ class EnhancedDataInput extends Component {
     // starts off the process of loading the data
     async PerformStep(dataset) {
         // call the function for the start step
-        this.state.steps[this.state.current]();
+        this.state.steps[this.state.current](dataset);
     }
 
     // increment the current step, and perform the next step
